2656fdd1ffee3b9eaebeae7d8d074949adba4cec
Lucas Gabriel <51632666+lucaslima0209@users.noreply.github.com>
2022-02-14 02:25:51 -0300

    att partitura/teclado

--- dist/js/teclado.js
+++ dist/js/teclado.js
@@ -1,3 +1,8 @@
+let partituras = document.querySelector(".partituras");
+let imagens = document.querySelectorAll(".img");
+let imgPrincipal = document.querySelector(".img-principal");
+let botaoAbrirPartituras = document.querySelector(".bt-partitura");
+
 const keys = document.querySelectorAll(".key"),
   note = document.querySelector(".nowplaying"),
   hints = document.querySelectorAll(".hints");
@@ -30,6 +35,41 @@ hints.forEach(hintsOn);
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
 window.addEventListener("keydown", playNote);
+
+function abrirPartituras(){
+  console.log(botaoAbrirPartituras.textContent);
+  if(botaoAbrirPartituras.textContent == 'abrir partituras'){
+    partituras.style.display = 'block';
+    botaoAbrirPartituras.textContent = 'fechar partituras'
+  }
+  else if(botaoAbrirPartituras.textContent == 'fechar partituras'){
+    partituras.style.display = 'none';
+    botaoAbrirPartituras.textContent = 'abrir partituras'
+  }
+  else if(botaoAbrirPartituras.textContent == 'fechar partitura'){
+    partituras.style.display = 'none';
+    imgPrincipal.style.display = "none";
+    botaoAbrirPartituras.textContent = 'abrir partituras'
+  }
+}
+
+imagens.forEach(imagem => {
+  imagem.addEventListener("click", function(){
+    if(imgPrincipal.lastChild != null){
+      imgPrincipal.removeChild(imgPrincipal.lastChild);
+    }
+    imgPrincipal.style.display = "block";
+    let src = imagem.src;
+    let img = document.createElement("img");
+    img.src = src;
+    img.style.maxHeight = '400px';
+    imgPrincipal.appendChild(img);
+    partituras.style.display = 'none';
+    botaoAbrirPartituras.textContent = 'fechar partitura'
+  })
+});
+
+
 
 //fonte: https://codepen.io/gabrielcarol/pen/rGeEbY
 //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
